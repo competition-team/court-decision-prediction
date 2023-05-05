d3b39a3350090805bd3b03b5e1eb1aa05b6284ce
alchemine <djyoon0223@gmail.com>
2023-05-06 00:03:51 +0900

    [update] Fix depth logging

--- base_project/common/depth_logging.py
+++ base_project/common/depth_logging.py
@@ -1,6 +1,6 @@
 """DepthLogger class.
 
-Log running level of processes.
+Log depth of code.
 """
 # Author: Dongjin Yoon <djyoon0223@gmail.com>
 
@@ -24,7 +24,7 @@ class DepthManager(contextlib.ContextDecorator):
 
     @classmethod
     def __exit__(cls, *exc):
-        cls.depths[cls.depth] = 1   # 다시 level을 1로 초기화
+        cls.depths[cls.depth] = 1   # reset
         cls.depth -= 1
         cls.depths[cls.depth] += 1
         return False
@@ -43,7 +43,7 @@ def D(fn):
         ...     # Depth: 2
         ...     # do something
     """
-    def print_fn(name, args, fn):
+    def _print_fn(name, args, fn):
         """Print depth of code.
 
         Args:
@@ -51,24 +51,15 @@ def D(fn):
             args (tuple): Arguments of the function
             fn (callable): Function
         """
-        logs = f"{(DepthManager.depth - 1) * ' ' + name:15}| "
-        if len(args) > 0 and isinstance(args[0], object):
+        logs = f"{'  ' + name:15}| "
+        if len(args) > 0 and isinstance(args[0], object):  # if function is method or main function
             logs = f"{logs}{fn.__module__.split('.')[-1]}."
         log(f"{logs}{fn.__name__}()")
 
     @wraps(fn)
     def _log(*args, **kwargs):
-        """Print depth of code.
-
-        Args:
-            args (tuple): Arguments of the function
-            kwargs (dict): Keyword arguments of the function
-
-        Returns:
-            Return value of the function
-        """
-        name = f"{'.'.join([str(DepthManager.depths[l]) for l in range(1, DepthManager.depth + 1)]):<10}"
-        print_fn(name, args, fn)
+        name = '.'.join([str(DepthManager.depths[d]) for d in range(1, DepthManager.depth + 1)])
+        _print_fn(name, args, fn)
 
         with DepthManager():
             with Timer(name):
--- base_project/common/timer.py
+++ base_project/common/timer.py
@@ -15,7 +15,7 @@ class Timer(contextlib.ContextDecorator):
         >>> from base_project.common.timer import Timer
         >>> with Timer('Code1'):
         ...     sleep(1)
-        * Code1 | 1.00s (0.02m)
+        * Code1        | 1.00s (0.02m)
     """
     def __init__(self, name='Elapsed time'):
         self.name = name
@@ -26,7 +26,7 @@ class Timer(contextlib.ContextDecorator):
 
     def __exit__(self, *exc):
         elapsed_time = perf_counter() - self.start_time
-        log(f"* {self.name:12} | {elapsed_time:.2f}s ({elapsed_time/60:.2f}m)")
+        log(f"{'* ' + self.name:15}| {elapsed_time:.2f}s ({elapsed_time/60:.2f}m)")
         return False
 
 
@@ -41,15 +41,6 @@ def T(fn):
     """
     @wraps(fn)
     def _log(*args, **kwargs):
-        """Code의 level을 출력
-
-        Args:
-            args (tuple): 함수의 arguments
-            kwargs (dict): 함수의 keyword arguments
-
-        Returns:
-            함수의 return value
-        """
         with Timer():
             rst = fn(*args, **kwargs)
         return rst
