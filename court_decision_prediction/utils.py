13b8b7301f4f8faba22263b7cb01b255ccc7476d
alchemine <djyoon0223@gmail.com>
2023-06-22 03:44:53 +0900

    [update] PATH

--- court_decision_prediction/utils.py
+++ court_decision_prediction/utils.py
@@ -4,6 +4,7 @@ from court_decision_prediction.common import *
 class PATH:
     root   = abspath(dirname(dirname(__file__)))
     yaml   = join(root, 'court_decision_prediction/configs.yaml')
-    train  = join(root, yaml2dict(yaml)['data_dir'], 'train.csv')
-    test   = join(root, yaml2dict(yaml)['data_dir'], 'test.csv')
-    submit = join(root, yaml2dict(yaml)['data_dir'], 'sample_submission.csv')
+    data   = join(root, yaml2dict(yaml)['data_dir'])
+    train  = join(data, 'train.csv')
+    test   = join(data, 'test.csv')
+    submit = join(data, 'sample_submission.csv')
